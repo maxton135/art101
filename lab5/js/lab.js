4c22219c79370debaa9761d6cb96bf60cb9a894a
maxton135 <maxton135@gmail.com>
2022-04-20 15:59:58 -0700

    changes

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+/**
+ * Author:    Maxton Lenox and Izetta Evans
+ * Created:   4.20.2022
+ * Assignment: Lab 5
+ *
+ **/
+
+// Define Variables
+make = "Toyota";
+model = "Camery";
+color = "Red";
+year = 2006;
+age = 2022 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
